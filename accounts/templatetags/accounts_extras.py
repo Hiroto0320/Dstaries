ba6e931ef0eb0aac6a8fabdea4f287fc167c11b7
Hiroto0320 <hirohirobouzu.2003@icloud.com>
2022-02-28 15:13:09 +0900

    diary image change every two times

--- accounts/templatetags/accounts_extras.py
+++ accounts/templatetags/accounts_extras.py
@@ -4,4 +4,6 @@ register = template.Library()
 
 @register.filter
 def join(arg1, arg2):
-    return str(arg1) + str(arg2) + '.png'
+    i = arg2*3
+    j = int(i/2)
+    return str(arg1) + str(j) + '.png'

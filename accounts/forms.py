fabda9bd8dfef4e2a7001dea31f9ed324e402589
Hiroto0320 <hirohirobouzu.2003@icloud.com>
2022-03-06 13:32:01 +0900

    try and error

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import User
 from django.contrib.auth.password_validation import validate_password
 class ImageForm(forms.ModelForm):
-    icon = forms.FileField(required=False)
+    icon = forms.FileField(required=False, widget=forms.FileInput)
     class Meta:
         model = User
         fields = ('icon',)

b389b2af20b9bd3e0b0d6cd7de40665cf4eefeec
Hiroto0320 <hirohirobouzu.2003@icloud.com>
2022-03-08 19:34:54 +0900

    try and error

--- accounts/templatetags/accounts_extras.py
+++ accounts/templatetags/accounts_extras.py
@@ -5,3 +5,10 @@ register = template.Library()
 @register.filter
 def join(arg1, arg2):
     return str(arg1) + str(arg2) + '.png'
+
+@register.filter
+def stories_is_max(arg1):
+    if arg1>=300:
+        return False
+    else:
+        return True

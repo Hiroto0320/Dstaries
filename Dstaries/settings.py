efa2250899c3ba20f12e5a1cbe8ad86f01c6a4f5
Hiroto0320 <hirohirobouzu.2003@icloud.com>
2022-03-09 15:02:08 +0900

    try and error

--- Dstaries/settings.py
+++ Dstaries/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 env = environ.Env(DEBUG=(bool,False))
-env.read_env('.env')
+env.read_env(BASE_DIR, '.env')
 DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = env('SECRET_KEY')
